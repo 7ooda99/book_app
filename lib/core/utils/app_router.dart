c51fff040075bceba9b18aad9ce7a319b6846bda
7ooda99 <mhd.alkayyal@gmail.com>
2023-04-01 02:51:33 +0300

    create custom appbar

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,10 +1,12 @@
 import 'package:book_app/features/home/presentation/views/home_view.dart';
+import 'package:book_app/features/home/presentation/views/widgets/book_details_view_body.dart';
 import 'package:go_router/go_router.dart';
 
 import '../../features/splash/presentation/views/splash_view.dart';
 
 abstract class AppRouter {
   static const kHomeView = '/homeView';
+    static const kDetailsScreen = '/detailsScreen';
   static final router = GoRouter(
     routes: [
       GoRoute(
@@ -14,7 +16,11 @@ abstract class AppRouter {
       GoRoute(
         path: kHomeView,
         builder: ((context, state) => const HomeView()),
-      )
+      ),
+      GoRoute(
+        path: kDetailsScreen,
+        builder: ((context, state) => const BookDetailsViewBody()),
+      ),
     ],
   );
 }
