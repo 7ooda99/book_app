46592b32419bb25b401987955e27eaa1e283cd22
7ooda99 <mhd.alkayyal@gmail.com>
2023-04-02 05:35:29 +0300

    refactor books details view

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -5,84 +5,45 @@ import 'package:book_app/features/home/presentation/views/widgets/custom_book_im
 import 'package:flutter/material.dart';
 
 import 'books_action.dart';
+import 'books_section.dart';
 import 'custom_book_details_app_bar.dart';
 import 'similar_books_list_view.dart';
+import 'similar_books_section.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
   const BookDetailsViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    var width = MediaQuery.of(context).size.width;
     return CustomScrollView(
       slivers: [
         SliverFillRemaining(
           hasScrollBody: false,
           child: SafeArea(
             child: Scaffold(
-                  body: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 30),
-            child: Column(
-              children: [
-                CustomBookDetailsAppBar(),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: width * .2),
-                  child: CustomBookImage(),
+              body: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30),
+                child: Column(
+                  children: [
+                    const CustomBookDetailsAppBar(),
+                    BookDetailsSection(),
+                    const Expanded(
+                      child: SizedBox(
+                        height: 50,
+                      ),
+                    ),
+                    const SimilarBooksSection()
+                  ],
                 ),
-                SizedBox(
-                  height: 43,
-                ),
-                Text(
-                  'Fortress Blood',
-                  style: Styles.textStyle30.copyWith(fontWeight: FontWeight.bold),
-                ),
-                SizedBox(
-                  height: 6,
-                ),
-                Opacity(
-                  opacity: .7,
-                  child: Text(
-                    'LD Goffiggan',
-                    style: Styles.textStyle18.copyWith(
-                        fontStyle: FontStyle.italic, fontWeight: FontWeight.w500),
-                  ),
-                ),
-                const SizedBox(
-                  height: 18,
-                ),
-                const BookRating(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                ),
-                const SizedBox(
-                  height: 37,
-                ),
-                const BookAction(),
-                const Expanded(
-                  child: SizedBox(
-                    height: 50,
-                  ),
-                ),
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    'You can also like',
-                    style:
-                        Styles.textStyle14.copyWith(fontWeight: FontWeight.w600),
-                  ),
-                ),
-                const SizedBox(
-                  height: 16,
-                ),
-                SimilarBooksListview()
-              ],
+              ),
             ),
-                  ),
-                ),
           ),
         )
       ],
     );
-
   }
 }
+
+
+
 
