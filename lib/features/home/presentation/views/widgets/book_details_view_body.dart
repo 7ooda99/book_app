9ca333f7dec50c4afcd6b371a6ef2075df0763e0
7ooda99 <mhd.alkayyal@gmail.com>
2023-04-02 04:33:38 +0300

    add custom scroll view

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -14,65 +14,75 @@ class BookDetailsViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var width = MediaQuery.of(context).size.width;
-    return SafeArea(
-      child: Scaffold(
-        body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 30),
-          child: Column(
-            children: [
-              CustomBookDetailsAppBar(),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: width * .2),
-                child: CustomBookImage(),
-              ),
-              SizedBox(
-                height: 43,
-              ),
-              Text(
-                'Fortress Blood',
-                style: Styles.textStyle30.copyWith(fontWeight: FontWeight.bold),
-              ),
-              SizedBox(
-                height: 6,
-              ),
-              Opacity(
-                opacity: .7,
-                child: Text(
-                  'LD Goffiggan',
-                  style: Styles.textStyle18.copyWith(
-                      fontStyle: FontStyle.italic, fontWeight: FontWeight.w500),
+    return CustomScrollView(
+      slivers: [
+        SliverFillRemaining(
+          hasScrollBody: false,
+          child: SafeArea(
+            child: Scaffold(
+                  body: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30),
+            child: Column(
+              children: [
+                CustomBookDetailsAppBar(),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: width * .2),
+                  child: CustomBookImage(),
                 ),
-              ),
-              const SizedBox(
-                height: 18,
-              ),
-              const BookRating(
-                mainAxisAlignment: MainAxisAlignment.center,
-              ),
-              const SizedBox(
-                height: 37,
-              ),
-              const BookAction(),
-              const SizedBox(
-                height: 30,
-              ),
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  'You can also like',
-                  style:
-                      Styles.textStyle14.copyWith(fontWeight: FontWeight.w600),
+                SizedBox(
+                  height: 43,
                 ),
-              ),
-              const SizedBox(
-                height: 16,
-              ),
-              SimilarBooksListview()
-            ],
+                Text(
+                  'Fortress Blood',
+                  style: Styles.textStyle30.copyWith(fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 6,
+                ),
+                Opacity(
+                  opacity: .7,
+                  child: Text(
+                    'LD Goffiggan',
+                    style: Styles.textStyle18.copyWith(
+                        fontStyle: FontStyle.italic, fontWeight: FontWeight.w500),
+                  ),
+                ),
+                const SizedBox(
+                  height: 18,
+                ),
+                const BookRating(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                ),
+                const SizedBox(
+                  height: 37,
+                ),
+                const BookAction(),
+                const Expanded(
+                  child: SizedBox(
+                    height: 50,
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'You can also like',
+                    style:
+                        Styles.textStyle14.copyWith(fontWeight: FontWeight.w600),
+                  ),
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                SimilarBooksListview()
+              ],
+            ),
+                  ),
+                ),
           ),
-        ),
-      ),
+        )
+      ],
     );
+
   }
 }
 
