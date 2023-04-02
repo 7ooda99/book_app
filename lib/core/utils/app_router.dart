84d0b8a566509a28969f436b3a8870dbe55cb482
7ooda99 <mhd.alkayyal@gmail.com>
2023-04-03 01:41:47 +0300

    create custom search text field

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,12 +1,14 @@
 import 'package:book_app/features/home/presentation/views/home_view.dart';
 import 'package:book_app/features/home/presentation/views/widgets/book_details_view_body.dart';
+import 'package:book_app/features/search/presentation/view/search_view.dart';
 import 'package:go_router/go_router.dart';
 
 import '../../features/splash/presentation/views/splash_view.dart';
 
 abstract class AppRouter {
   static const kHomeView = '/homeView';
-    static const kDetailsScreen = '/detailsScreen';
+  static const kDetailsScreen = '/detailsScreen';
+  static const kSearchView = '/searchView';
   static final router = GoRouter(
     routes: [
       GoRoute(
@@ -20,6 +22,10 @@ abstract class AppRouter {
       GoRoute(
         path: kDetailsScreen,
         builder: ((context, state) => const BookDetailsViewBody()),
+      ),
+      GoRoute(
+        path: kSearchView,
+        builder: ((context, state) => const SearchView()),
       ),
     ],
   );
--- lib/features/home/presentation/views/widgets/custom_app_bar.dart
+++ lib/features/home/presentation/views/widgets/custom_app_bar.dart
@@ -1,5 +1,7 @@
+import 'package:book_app/core/utils/app_router.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:go_router/go_router.dart';
 
 import '../../../../../core/utils/assets.dart';
 
@@ -9,12 +11,23 @@ class CustomAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only( right: 24, top: 40, bottom: 20),
+      padding: const EdgeInsets.only(right: 24, top: 40, bottom: 20),
       child: Row(
         children: [
-          Image.asset(AssetsData.logo, height: 20,),
+          Image.asset(
+            AssetsData.logo,
+            height: 20,
+          ),
           const Spacer(),
-          IconButton(onPressed: (){}, icon: const Icon(FontAwesomeIcons.magnifyingGlass, size: 24,))
+          IconButton(
+            onPressed: () {
+              GoRouter.of(context).push(AppRouter.kSearchView);
+            },
+            icon: const Icon(
+              FontAwesomeIcons.magnifyingGlass,
+              size: 22,
+            ),
+          )
         ],
       ),
     );
